b91c0a9449fa1c478a2242a670ecd912d99c30ba
Aleksey Martishevskiy <martkst@yandex.ru>
2019-10-13 23:15:43 +0300

    My colution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        
     }
 
     guess() {
-
+        return Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.setRange(this.min, Math.round((this.min + this.max) / 2));
     }
 
     greater() {
-
+        this.setRange(Math.round((this.min + this.max) / 2), this.max);
     }
 }
 
 module.exports = GuessingGame;
+
+
